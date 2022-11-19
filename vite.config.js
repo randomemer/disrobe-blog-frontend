7fea97908d3738a1136d175cef62f11081a0f777
randomemer <pshashank569@gmail.com>
2022-11-19 09:03:42 +0530

    migrated from CRA to vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+process.env.BROWSER = "firefox";
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	server: {
+		port: 3000,
+		strictPort: true,
+	},
+});
