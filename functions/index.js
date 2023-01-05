af6f5c7cba87950111a4db38a3c728b61b349356
randomemer <pshashank569@gmail.com>
2023-01-05 21:43:57 +0530

    deployed cloud function cleanStoryImages

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,30 @@ export const cleanStoryImages = functions
 		const bucket = admin.storage().bucket();
 
 		const collection = await firestore.collection("stories").get();
+		const images = new Set();
 
-		collection.docs.forEach(async (doc) => {});
+		for (const doc of collection.docs) {
+			let draft = await doc.get("data.draft.content");
+			draft = JSON.parse(draft);
+			for (const element of draft) {
+				if (element.type === "image" && element.source_type === "local") {
+					images.add(element.bucket_path);
+				}
+			}
+		}
+
+		const [files] = await bucket.getFiles({
+			prefix: "images/stories/",
+			delimiter: "/",
+		});
+
+		for (const file of files) {
+			if (
+				file.metadata.contentType.startsWith("image") &&
+				!images.has(file.name)
+			) {
+				await file.delete();
+				functions.logger.log("deleted file :", file.name);
+			}
+		}
 	});
